da8380750d8ec84e56a38ea3bafeb4dcfc7e7dba
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2022-10-05 07:22:54 +0530

    chore: remove un-necessary code

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,1 @@
-export const sayHello = (name) => `Hello, ${name}!`
+export const sayHello = (name: string) => `Hello, ${name}!`
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -2,13 +2,9 @@ import { test, expect, it } from "vitest";
 import * as nodeFetch from "node-fetch"
 import { sayHello } from '../src'
 
-// eslint-disable-next-line
 (globalThis as any).fetch = nodeFetch.default;
-// eslint-disable-next-line
 (globalThis as any).Request = nodeFetch.Request;
-// eslint-disable-next-line
 (globalThis as any).Headers = nodeFetch.Headers;
-// eslint-disable-next-line
 (globalThis as any).Response = nodeFetch.Response;
 
 test("pkg", async () => {
